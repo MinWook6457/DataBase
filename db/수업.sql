ab9afedd3e0bb4f25c95f7cc077b367716863cb1
MinWook6457 <minwook6457@gmail.com>
2022-10-09 20:41:44 +0900

    DB 실습

--- db/수업.sql
+++ db/수업.sql
@@ -319,3 +319,93 @@ delete from book where publisher = "이상미디어";
 
 -- (4) 출판사 '대한미디어'가 '대한출판사'로 이름을 바꾸었다.
 update book set publisher = '대한출판사' where publisher = '대한미디어';
+
+-- select bookname from book where price > (select avg(price) from book where publisher = publisher);
+
+select custid '고객번호' , round(sum(saleprice)/count(*),-2) '평균금액' 
+from orders
+group by custid;
+
+select bookid, replace(bookname,'야구','농구') bookname,publisher,price from book;
+
+select bookname '제목', char_length(bookname)'문자 수' , length(bookname) from book where publisher = '굿스포츠';
+
+select substr(name,1,1) '성', count(*)'인원' from customer group by substr(name,1,1); 
+
+select orderid '주문번호' , orderdate '주문일' , adddate(orderdate,interval 10 day)'확정' from orders;
+
+select orderid '주문번호' , str_to_date(orderdate,'%Y-%m-%d') '주문일', custid '고객번호', bookid '도서번호' from orders where orderdate = date_format('20140707','%Y-%m-%d'); 
+
+select sysdate(), date_format(sysdate(),'%Y/%m/%d %M %h:%s') 'sysdate_1';
+
+select price+100 from mybook where bookid=3;
+
+select sum(price), avg(price), count(*), count(price) from mybook;
+
+select sum(price), avg(price), count(*) from mybook where bookid >= 4;
+
+select name,bookname from customer,book,orders where customer.custid = orders.custid and orders.bookid = book.bookid; 
+
+select name,bookname from book,orders,customer where price = 20000 and customer.custid = orders.custid and book.bookid = orders.bookid;
+
+select name,saleprice from orders right outer join customer on customer.custid = orders.custid;
+
+select bookname from book where price = (select max(price) from book );
+
+select name from customer where custid in (select custid from orders where bookid in(select bookid from book where publisher = '대한출판사'));
+
+select b1.bookname from book b1 where b1.price > (select avg(b2.price) from book b2 where b2.publisher = b1.publisher);
+
+select name , address from customer c where exists (select * from orders o where c.custid = o.custid);
+
+-- set sql_safe_updates = 0;
+update customer set address ='대한민국 부산' where custid = 3;
+select * from customer;
+
+update book set publisher = (select publisher from imported_book where bookid = 21) where bookid = 7;
+select * from book;
+
+select distinct name from customer,book,orders where customer.custid = orders.custid and book.bookid = orders.bookid and name not in ('박지성')
+and book.publisher in (select distinct book.publisher from book,orders where book.bookid = orders.bookid );
+
+select distinct customer.name 
+from customer , orders , book 
+where customer.custid = orders.custid 
+and book.bookid = orders.bookid 
+group by customer.name 
+having count(distinct publisher)>= 2;
+
+-- 4-1) -78과 18의 절댓값을 구하시오.
+select abs(-78),abs(+78);
+
+-- 4-2) 4.875를 소수 첫째 자리까지 반올림한 값을 구하시오.
+select round(4.875,1);
+
+-- 4-3) 고객별 평균 주문 금액을 백원 단위로 반올림한 값을 구하시오.
+select custid '고객번호' , round(sum(saleprice)/count(*),-2) '평균금액' 
+from orders
+group by custid;
+
+-- 4-4) 도서 제목에 야구가 포함된 도서를 농구로 변경한 수 도서목록을 보이시오.
+select bookid, replace(bookname,'야구','농구') bookname,publisher,price from book;
+
+-- 4-5) 굿스포츠에서 출간한 도서의 제목과 제목의 문자수,바이트 수를 보이시오.
+select bookname '제목', char_length(bookname)'문자 수' , length(bookname) from book where publisher = '굿스포츠';
+
+ -- 4-6) 마당서점의 고객 중에서 같은 성을 가진 사람이 몇 명이나 되는지 성별 인원수를 구하시오.
+select substr(name,1,1) '성', count(*)'인원' from customer group by substr(name,1,1);
+
+ -- 4-7) 마당 서점은 주문일로부터 10일 후 매출을 확정한다. 각 주문의 확정일자를 구하시오.
+select orderid '주문번호' , orderdate '주문일' , adddate(orderdate,interval 10 day)'확정' from orders;
+
+-- 4-8) 마당서점이 2014.07.07에 주문받은 도서를 모두 보이시오.
+select orderid '주문번호' , str_to_date(orderdate,'%Y-%m-%d') '주문일', custid '고객번호', bookid '도서번호' from orders where orderdate = date_format('20140707','%Y-%m-%d'); 
+
+-- 4-9) DBMS 서버에 현재 설정된 날짜와 시간 요일 확인하시오.
+select sysdate(), date_format(sysdate(),'%Y/%m/%d %M %h:%s') 'sysdate_1'; 
+
+select price+100 from mybook where bookid=3;
+
+select sum(price), avg(price), count(*), count(price) from mybook;
+
+select sum(price), avg(price), count(*) from mybook where bookid >= 4;
